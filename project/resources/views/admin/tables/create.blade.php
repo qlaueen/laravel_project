f6f9309c0cf4e359709bde829ec06c3552cb1fbc
laura <qlaueen@gmail.com>
2022-07-19 12:39:06 -0400

    store new table

--- project/resources/views/admin/tables/create.blade.php
+++ project/resources/views/admin/tables/create.blade.php
@@ -1,42 +1,58 @@
 <x-admin-layout>
   <x-slot name="header">
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          {{ __('Dashboard') }}
-      </h2>
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+      {{ __('Dashboard') }}
+    </h2>
   </x-slot>
 
   <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="flex justify-end m-2 p-2">
-            <a href="{{  route('admin.tables.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-lg">View all tables</a>
-          </div>
-          <div class="m-2 p-2 bg-slate-100 rounded">
-              <form enctype="multipart/form-data">
-                <div class="sm:col-span-6">
-                  <label for="title" class="block text-sm font-medium text-gray-700"> Name </label>
-                  <div class="mt-1">
-                    <input type="text" id="title" wire:model.lazy="title" name="title" class="block w-full transition ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal duration-150 sm:text-sm sm:leading-5" />
-                  </div>
-                </div>
-                <div class="sm:col-span-6">
-                  <label for="title" class="block text-sm font-medium text-gray-700"> Image </label>
-                  <div class="mt-1">
-                    <input type="file" id="image" wire:model="newImage" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal  sm:text-sm sm:leading-5" />
-                  </div>
-                </div>
-                <div class="sm:col-span-6 pt-5">
-                  <label for="body" class="block text-sm font-medium text-gray-700">Description</label>
-                  <div class="mt-1">
-                    <textarea id="body" rows="3" wire:model.lazy="body" class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm "></textarea>
-                  </div>
-                </div>
-                <div class="mt-6">
-                  <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Store</button>
-                </div>
-              </form>
-            </div>            
-          </div>
-      
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="flex justify-end m-2 p-2">
+        <a href="{{  route('admin.tables.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-lg">View all tables</a>
       </div>
+      <div class="m-2 p-2 bg-slate-100 rounded">
+        <form method="POST" action="{{ route('admin.tables.store') }}">
+          @csrf
+          <div class="sm:col-span-6">
+            <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+            <div class="mt-1">
+              <input type="text" id="name" name="name" 
+                    class="block w-full transition ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal duration-150 sm:text-sm sm:leading-5"/>
+            </div>
+          </div>
+          <div class="sm:col-span-6">
+            <label for="guest_number" class="block text-sm font-medium text-gray-700">Guest Number</label>
+            <div class="mt-1">
+              <input type="number" id="guest_number" name="guest_number" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal  sm:text-sm sm:leading-5"/>
+            </div>
+          </div>
+          <div class="sm:col-span-6 pt-5">
+            <label for="status" class="block text-sm font-medium text-gray-700">Status</label>
+            <div class="mt-1">
+              <select id="status" name="status" class="form-multiselect block w-full mt-1">
+                @foreach (App\Enums\TableStatus::cases() as $location)  
+                  <option value="{{ $location->value }}">{{ $location->name }}</option>
+                @endforeach
+              </select>
+            </div>
+          </div>
+          <div class="sm:col-span-6 pt-5">
+            <label for="location" class="block text-sm font-medium text-gray-700">Location</label>
+            <div class="mt-1">
+              <select id="location" name="location" class="form-multiselect block w-full mt-1">
+                @foreach (App\Enums\TableLocation::cases() as $location)  
+                  <option value="{{ $location->value }}">{{ $location->name }}</option>
+                @endforeach
+              </select>
+            </div>
+          </div>
+          <div class="mt-6">
+            <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Store</button>
+          </div>
+            </form>
+          </div>            
+        </div>
+    
+    </div>
   </div>
 </x-admin-layout>
